70e61141f5c54c3aed15f77e7dc43946fa449622
davigomesflorencio <davigomesflorencio@gmail.com>
2026-02-05 09:43:11 -0300

    Initial Commit

---
+++ gnix_touchdown/gnix_touchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  gnix_touchdownApp.swift
+//  gnix_touchdown
+//
+//  Created by Davi Gomes Florencio on 05/02/26.
+//
+
+import SwiftUI
+
+@main
+struct gnix_touchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
